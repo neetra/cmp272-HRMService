13b6f63d1fe8dddfbe64a37ef920ed5f0dd19d2a
Netra Amrale <30404609+neetra@users.noreply.github.com>
2021-11-14 10:53:21 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 PROJECT_NAME = "Hakkuna_Matata_HRM"
 GOOGLE_CLIENT_ID = "1017420389855-8imp6dckcrna8f100sbskn21bkqhf8su.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_CLIENT_SECRET = ""
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
@@ -10,5 +10,5 @@ GOOGLE_DISCOVERY_URL = (
 db_host = "hrm-db1.ciunuysgnwmp.us-west-1.rds.amazonaws.com"
 db_database ="hrm_db"
 db_username="admin"
-db_password = "LRaOA05sEpntp2mXyo6d"
+db_password = ""
 
